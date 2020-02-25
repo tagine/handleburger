7902e3a3718102a12de91ca481847b6e59e9dd1e
tagine <rana@ranafayez.com>
2020-02-24 22:33:23 -0500

    schema and seeds updated

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name) VALUES ('Mushroom Swiss');
+INSERT INTO burgers (burger_name) VALUES ('Big Mac');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Grilled Chicken', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Fried Chicken', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Brisket', true);
+INSERT INTO burgers (burger_name) VALUES ('Radish Bison');
