2edf58ed060734970d88a91e2c8f33ff9da8a1c4
tagine <rana@ranafayez.com>
2020-02-29 18:42:12 -0500

    burgerlogic fixed for burgerblock buttons and some variables adjusted to es6 standards

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-var express = require("express");
+const express = require("express");
 
-var PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8080;
 
-var app = express();
+const app = express();
 
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("public"));
